db81cdbc01eadf85baf0c51a7a826d6073424a7c
Yonathan <jrizky.jonathan@gmail.com>
2020-02-23 21:41:53 +0700

    23022020

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -11,8 +11,8 @@
         <div class="card-body">
         <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Transaksi Pembelian</div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $totalPembelian; ?></div>
+            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Data</div>
+            <div class="h5 mb-0 font-weight-bold text-gray-800">0</div>
             </div>
             <div class="col-auto">
             <i class="fas money-bill-alt fa-2x text-gray-300"></i>
@@ -28,8 +28,8 @@
         <div class="card-body">
         <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Transaksi Penjualan</div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $totalPenjualan; ?></div>
+            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Data</div>
+            <div class="h5 mb-0 font-weight-bold text-gray-800">0</div>
             </div>
             <div class="col-auto">
             <i class="fas money-bill-wave fa-2x text-gray-300"></i>
@@ -45,8 +45,8 @@
         <div class="card-body">
         <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Pembatalan Transaksi Pembelian</div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $totalPembatalanPembelian; ?></div>
+            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Data</div>
+            <div class="h5 mb-0 font-weight-bold text-gray-800">0</div>
             </div>
             <div class="col-auto">
             <i class="fas money-bill-alt fa-2x text-gray-300"></i>
@@ -62,8 +62,8 @@
         <div class="card-body">
         <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Total Pembatalan Transaksi Penjualan</div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $totalPembatalanPenjualan; ?></div>
+            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Total Data</div>
+            <div class="h5 mb-0 font-weight-bold text-gray-800">0</div>
             </div>
             <div class="col-auto">
             <i class="fas money-bill-alt fa-2x text-gray-300"></i>
